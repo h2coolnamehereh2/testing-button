2323d260404a61f124d8d87611e7defd97d013fb
Svilen <svilenb.gergov@gmail.com>
2024-01-17 15:08:06 +0200

    change msg

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'hey it worked as expected'
+    return 'hey it worked as expected lol amazing'
 
 @app.errorhandler(500)
 def server_error(e):
